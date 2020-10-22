836b3194881bc25d016c6329e244f5d0ddb47a70
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-23 07:43:50 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var current = "o";
+var i = 0;
+var divs = document.getElementsByTagName("div");
 function clicked(me) {
-  me.innerText = current;
-  if (current == "o") {
-    current = "x";
+  if(!me.innerText) {
+    me.innerText = current;
+    if (current == "o") {
+      current = "x";
+      document.getElementById("t").innerText = "It is x's turn";
+    }
+    else {
+      current = "o";
+      document.getElementById("t").innerText = "It is o's turn";
+    }
   }
-  else {
-    current = "o";
+}
+function resetall() {
+  for (var i = 0; i < 64; i++) {
+    divs[i].innerText = "";
   }
 }
