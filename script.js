cd5082eab0304e1bb9453cc0193b0a7d312f6b33
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-23 11:13:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 var current = "o";
 var i = 0;
-var divs = document.getElementsByTagName("div");
 function clicked(me) {
   if(!me.innerText) {
     me.innerText = current;
+    document.getElementById("t").innerText = "Current turn: " + current;
     if (current == "o") {
       current = "x";
-      document.getElementById("t").innerText = "It is x's turn";
     }
     else {
       current = "o";
-      document.getElementById("t").innerText = "It is o's turn";
     }
   }
 }
 function resetall() {
   for (var i = 0; i < 64; i++) {
-    divs[i].innerText = "";
+    document.getElementsByTagName("div")[i].innerText = "";
   }
 }
